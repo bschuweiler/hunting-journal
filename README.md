1bcf4ccd7f4874428e8fbeca1072328f4691a925
Brian Schuweiler <brian.schuweiler@thomsonreuters.com>
2017-09-29 12:59:34 -0500

    More initial additions

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple waterfowl hunting journal ReST service
--- app.py
+++ app.py
@@ -10,7 +10,7 @@ _db = 'sqlite:///testdata.db'
 _dbEngine = create_engine(_db)
 _Session = sessionmaker(bind=_dbEngine)
 
-app = Chalice(app_name='python-web-service')
+app = Chalice(app_name='hunting-journal')
 app.debug = True
 
 """
@@ -94,25 +94,3 @@ def getBirds():
 @app.route('/hunters', methods=['GET'])
 def getHunters():
     return getAllResources(Hunter)
-
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
